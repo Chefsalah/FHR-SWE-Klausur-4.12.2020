a12b6e384a118c5df58198703f65f9a64404263f
Chefsalah <salahadin.a@gso.schule.koeln>
2021-12-04 00:51:14 +0100

    first commit

---
+++ FHR-Tests/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FHR;
+
+namespace FHR_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Bruttopreis_berechnetsichAusBettopreisUndMwst()
+        {
+            //Arrange
+            ProduktNachschreiber P = new ProduktNachschreiber();
+            P.Bruttopreis = 10.00;
+            P.Mwst = 0.19;
+            //Act
+            double ergebniss = P.NettoPreis();
+
+            //Assert
+            Assert.AreEqual(11.90, ergebniss);
+        }
+
+
+        [TestMethod]
+        public void Rabbatieren_senktBruttopreis()
+        {
+            //arrange
+            ProduktNachschreiber P = new ProduktNachschreiber();
+            P.Bruttopreis = 10.00;
+
+            //act
+            P.Rabbatieren(20);
+
+            //Assert
+            Assert.AreEqual(8.0, P.Bruttopreis);
+        }
+        [TestMethod]
+        public void Rabbatien_zuHoherWertWirdIgnoriert()
+        {
+            //Arrange
+            ProduktNachschreiber P = new ProduktNachschreiber();
+            P.Bruttopreis = 10.00;
+
+            //Act 
+            P.Rabbatieren(90);
+
+            //Assert
+            Assert.AreEqual(10.00,P.Bruttopreis);
+        }
+
+    }
+}
---
+++ FHR/ProduktNachschreiber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHR
+{
+    public class ProduktNachschreiber
+    {
+        private double bruttopreis;
+        private double mwst;
+        private double nettopreis;
+        public double NettoPreis()
+        {
+            nettopreis = bruttopreis + bruttopreis * mwst;
+            return nettopreis;
+        }
+
+        public void Rabbatieren(double rabbatInProzent)
+        {
+            if (rabbatInProzent < 90)
+            {
+                bruttopreis =bruttopreis - bruttopreis * (rabbatInProzent / 100);
+            }
+
+        }
+        public double Bruttopreis { get { return bruttopreis; } set { bruttopreis = value; } }
+        public double Mwst { get { return mwst; } set { mwst = value; } }
+    }
+}
